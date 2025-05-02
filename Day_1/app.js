2f8a2fccfa270ea88cd4e3261e70ae514a253344
emran71 <ekhan4475@gmail.com>
2025-05-03 02:25:46 +0600

    Day 1 task completed

---
+++ Day_1/app.js
@@ -0,0 +1,4 @@
+console.log("Hello, tapaScript!");
+console.log("Welcome to 40 Days of JavaScript!");
+document.write("Check the browser console for a message!"); 
+// error:  A call to document.write() from an asynchronously-loaded external script was ignored.
