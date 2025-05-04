fb70596ea7b869678e1e11cdfe09123797bdee19
emran71 <ekhan4475@gmail.com>
2025-05-05 01:56:36 +0600

    trask 2 Completed for day 2

---
+++ Day_2/script.js
@@ -0,0 +1,34 @@
+let name ="Emran Khan";
+let age = 25;
+let isStudent = true;
+let programmingLanguages = "Javascript";
+
+// Output
+// Emran Khan
+// 25
+// true
+// Javascript
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(programmingLanguages);
+// 
+let age = 25;
+let isStudent = true;
+let programmingLanguages = "Javascript";
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(programmingLanguages);
+
+// Identifier 'age' has already been declared
+const user ={
+    name: "Emran Khan",
+    age: 25,
+    isStudent: true,
+    programmingLanguages: "Javascript"
+}
+let arr = [1, 2, 3, 4, 5];
+console.log(user);
+console.log(arr);
